d0bcd7e80f8b02f8ceebcf4f277dcbb2e8c5a5d2
PavelSteep <141700339+PavelSteep@users.noreply.github.com>
2025-03-15 19:03:22 +0100

    15.03.25-2

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -5,13 +5,3 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = join(__filename, "..");
 
 export const PATH_DB = join(__dirname, "../db/db.json");
-export const ENV_VARS = {
-  PORT: '3000',
-};
-export const PORT = 3000;
-export const HOST = "localhost";
-export const BASE_URL = `http://${HOST}:${PORT}`;
-export const BASE_API = `${BASE_URL}/api`;
-export const BASE_API_V1 = `${BASE_API}/v1`;
-export const BASE_API_V1_CONTACTS = `${BASE_API_V1}/contacts`;
-export const BASE_API_V1_CONTACTS_ID = `${BASE_API_V1_CONTACTS}/:id`;
--- src/utils/getEnvVar.js
+++
@@ -1,7 +0,0 @@
-export const getEnvVar = (envVarName, defaultValue) => {
-  // Если переменная окружения не найдена, вернуть значение по умолчанию
-  if (!defaultValue) {
-    throw new Error(`Missing env var ${envVarName}`);
-  }
-  return defaultValue;
-};
